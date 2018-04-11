1c608c2389bda85847c853698edd8b2b749005bc
TOMWORK\Administrator <gaozichen2012@outlook.com>
2018-04-11 16:32:42 +0800

    [V1.01.02.017] cctl=6000,5000,0 黄知青曲线1

--- SRC/Syetem/KeyCmd.c
+++ SRC/Syetem/KeyCmd.c
@@ -10,8 +10,8 @@ typedef struct {							//mcu version information data struct
 
 static const MCU_VERSION Mcu_Version = {	//mcu vwersion information
 	{"A508"},				//product version
-	{0x10,0x10,0x20,0x16},
-	{0x20,0x18,0x04,0x10},
+	{0x10,0x10,0x20,0x17},
+	{0x20,0x18,0x04,0x11},
 	{"res"},				//4//4byte eeprom version
 	{"res"}
 };
